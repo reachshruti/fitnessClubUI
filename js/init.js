466a8e2ae8dedbf35aefefa5b47326a32c2230c9
[reachshruti] <shrutiagarwal.reach@gmail.com>
2018-02-27 00:44:35 +0530

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+		$('select').material_select();
+		$(".dropdown-button").dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
